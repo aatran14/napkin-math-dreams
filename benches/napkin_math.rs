53c1bb3617f79aac1af84e911d0784faba50a75c
aatran14 <tranandy012@gmail.com>
2026-06-05 21:27:26 -0700

    clean: strip gh-pages to site files only

--- benches/napkin_math.rs
+++
@@ -1,9 +0,0 @@
-mod benchmarks;
-#[macro_use]
-extern crate byte_unit;
-use criterion::criterion_main;
-
-criterion_main! {
-    benchmarks::memory_read::benches,
-    benchmarks::compressed_memory_read::benches,
-}
